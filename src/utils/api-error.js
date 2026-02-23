19b03194ae3d52293465b06b5ee7d713c60aa52c
Priyanshu Pratik <pratikpriyanshu004@gmail.com>
2026-02-23 20:49:19 +0530

    honestly at this point of time i do not understand a few things i hope it comes to be by the end

---
+++ src/utils/api-error.js
@@ -0,0 +1,21 @@
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    message = "Something went wrong",
+    errors = [],
+    stack = "", // allows us to maintain the exact history of the error
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.message = message;
+    this.success = false;
+    this.errors = errors;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
