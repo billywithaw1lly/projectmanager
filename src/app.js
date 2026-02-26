c5b5a7280bc9c2324d0a6a5b85e0a725a391f250
Priyanshu Pratik <pratikpriyanshu004@gmail.com>
2026-02-26 09:08:52 +0530

    commit from feb 26

--- src/app.js
+++ src/app.js
@@ -1,8 +1,26 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
-app.use(express.json({limit: "16kb"}))
+// basic configuration
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public")); // serving public src
+
+//cors configurations
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Authorization", "Content-Type"],
+  }),
+);
+
+import healthCheckRouter from "./routes/healthcheck.routes.js";
+
+app.use("/api/v1/healthcheck", healthCheckRouter);
 
 app.get("/", (req, res) => {
   res.send("welcome to base");
