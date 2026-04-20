95738d05da461eff34d8003bea4d89b4363ef9e7
Priyanshu Pratik <pratikpriyanshu004@gmail.com>
2026-04-21 04:07:00 +0530

    wrote validators

---
+++ src/validators/index.js
@@ -0,0 +1,89 @@
+import {body} from "express-validator";
+import { AvailableUserRole } from "../utils/constants.js";
+
+const userRegisterValidator = () => {
+    return [
+        body("email")
+            .trim()
+            .notEmpty()
+            .withMessage("email is required")
+            .isEmail()
+            .withMessage("email is invalid"),
+        body("username")
+            .trim()
+            .notEmpty()
+            .withMessage("username is required")
+            .isLowercase()
+            .withMessage("username must be lowercase")
+            .isLength({min: 3})
+            .withMessage("Username must be at least 3 characters long"),
+        body("password")
+            .trim()
+            .notEmpty()
+            .withMessage("Password is required"),
+        body("fullName")
+            .optional()
+            .trim(),
+    ];
+};
+
+const userLoginValidator = () => {
+    return [
+        body("email").optional().isEmail().withMessage("Email is invalid"),
+        body("password").notEmpty().withMessage("Password is required"),
+    ]
+}
+
+const userChangeCurrentPasswordValidator = () => {
+  return [
+    body("oldPassword").notEmpty().withMessage("Old password is required"),
+    body("newPassword").notEmpty().withMessage("New password is required"),
+  ];
+};
+
+const userForgotPasswordValidator = () => {
+  return [
+    body("email")
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Email is invalid"),
+  ];
+};
+
+const userResetForgotPasswordValidator = () => {
+  return [body("newPassword").notEmpty().withMessage("Password is required")];
+};
+
+const createProjectValidator = () => {
+  return [
+    body("name").notEmpty().withMessage("Name is required"),
+    body("description").optional(),
+  ];
+};
+
+const addMembertoProjectValidator = () => {
+  return [
+    body("email")
+      .trim()
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Email is invalid"),
+    body("role")
+      .notEmpty()
+      .withMessage("Role is required")
+      .isIn(AvailableUserRole)
+      .withMessage("Role is invalid"),
+  ];
+};
+
+export {
+  userRegisterValidator,
+  userLoginValidator,
+  userChangeCurrentPasswordValidator,
+  userForgotPasswordValidator,
+  userResetForgotPasswordValidator,
+  createProjectValidator,
+  addMembertoProjectValidator,
+};
